e3bcf9c1751adddd7d2648fd6e9de34bef0dd3ae
edoorinaldoo@gmail.com <edoorinaldoo@gmail.com>
2022-08-26 12:54:06 +0700

    update

--- resources/views/pages/men.blade.php
+++ resources/views/pages/men.blade.php
@@ -7,12 +7,12 @@
 @section('content')
 <section>
     <div class="container">
-        <div class="category-title py-5 px-10">
+        <div class="category-title py-1 px-1">
             <h5 class="text-center fs-2">Men's Clothing</h5>
         </div>
         <div class="row">
             @foreach ($datas as $data)
-            <div class="col-lg-3 col-md-4 col-6 mt-4">
+            <div class="col-lg-3 col-md-4 col-6 mt-4 col-card-index">
                 <div class="card card-index" style="width: 18rem;">
                     <img src="{{ $data->image }}" class="card-img-top p-3" alt="{{ $data->title }}">
                     <div class="card-body">
